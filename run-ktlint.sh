5a63d53f48dbc34ab17e93e9b813f67a5b558ce7
Stephen <stephen@wdbible.org>
2022-04-14 09:18:53 +0700

    Added ktlint to pre-commit

---
+++ run-ktlint.sh
@@ -0,0 +1,11 @@
+#!/bin/sh
+set -e -o pipefail
+
+if ! command -v ktlint &> /dev/null ; then
+    echo "ktlint not installed or available in the PATH" >&2
+    exit 1
+fi
+
+# https://github.com/pinterest/ktlint pre-commit hook
+git diff --name-only --cached --relative | grep '\.kt[s"]\?$' | xargs ktlint --relative
+if [ $? -ne 0 ]; then exit 1; fi
